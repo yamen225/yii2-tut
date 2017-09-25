a1cfeb647c181d707e6767630eca1dd1eca73886
yamen <yamen225@gmail.com>
2017-09-25 18:01:01 +0200

    first changes

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -123,4 +123,9 @@ class SiteController extends Controller
     {
         return $this->render('about');
     }
+    public function actionHello()
+    {
+      $name = 'Yamen';
+      return $this->render('hello',array('name'=>$name));
+    }
 }
